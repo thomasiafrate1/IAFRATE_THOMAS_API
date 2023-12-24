fa2e2c92bc3dea5adc83f3712a5ba8d5de644a56
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-12-24 16:11:55 +0100

    API

---
+++ www/IAFRATE_THOMAS_API/creditials/mdp.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace credentials;
+
+class mdp
+{
+    public static function GetCredentials($fileName){
+
+        $path = $_SERVER['DOCUMENT_ROOT'] . '/../mdp/' . $fileName;
+
+        return json_decode(file_get_contents($path));
+
+    }
+}
---
+++ www/IAFRATE_THOMAS_API/public/index.php
@@ -0,0 +1,16 @@
+<?php
+require_once '..\src\Lib\Sql.php';
+use src\Lib\Sql;
+
+try {
+    $action = $_GET['action'] ?? '';
+    $id = $_GET['id'] ?? null;
+    $nom = $_GET['nom'] ?? '';
+    $prenom = $_GET['prenom'] ?? '';
+    $age = $_GET['age'] ?? null;
+
+    $resultat = Sql::gererAction($action, $id, $nom, $prenom, $age);
+    echo $resultat;
+} catch (Exception $e) {
+    echo "Erreur: " . $e->getMessage();
+}
