b491db8cadcaa082bf006529669df315c072f019
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-12-24 16:17:00 +0100

    FINAL

--- www/IAFRATE_THOMAS_API/credential/mdp.php
+++ www/IAFRATE_THOMAS_API/credential/mdp.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace credentials;
+namespace credential;
 
 class mdp
 {
--- www/IAFRATE_THOMAS_API/credential/toto.php
+++ www/IAFRATE_THOMAS_API/credential/toto.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once($_SERVER['DOCUMENT_ROOT'] . "/IAFRATE_THOMAS_API/credentials/mdp.php");
-$cred = credentials\mdp::GetCredentials("credentials.json");
+$cred = credential\mdp::GetCredentials("credentials.json");
 echo $cred->username . "<br>" . $cred->password;
 
 $dbh = new PDO("mysql:host=localhost;dbname=iafrate_thomas_api", $cred->username, $cred->password);
